f69cfcfe3a279fe92ed03a13bcf7685fb8e8bb91
岳霖 <jixiang_y@126.com>
2018-09-26 10:03:27 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import showEllipsis from './utils/showEllipsis.js';
+
+export {
+  showEllipsis,
+};
+
+export default {
+  showEllipsis,
+};
---
+++ src/utils/showEllipsis.js
@@ -0,0 +1,21 @@
+/**
+ * 超出一定字数显示...
+ * @param {*} str 
+ */
+function showEllipsis(str, length) {
+  if (!str) {
+    return;
+  }
+  if (typeof str !== 'string' || !length) {
+    return str;
+  }
+  if (Number(length) === NaN) {
+    return str;
+  }
+  if (str.length < length || str.length == length) {
+    return str;
+  }
+  return str.substr(Number(length), length) + '...';
+}
+
+export default showEllipsis;
