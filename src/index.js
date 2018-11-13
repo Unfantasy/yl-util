0210086748206b0e93167e47901416f749ece99e
岳霖 <jixiang_y@126.com>
2018-11-13 16:11:54 +0800

    add log function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import showEllipsis from './utils/showEllipsis.js';
 import pointer from './utils/pointer.js';
 
+/**
+ * 将传入的值通过JSON parse 深拷贝之后打印出来
+ * @param {*} param 
+ */
+export function log(param) {
+  if (typeof param !== 'object') {
+    console.log(param);
+    return param;
+  }
+  console.log(JSON.parse(JSON.stringify(param)));
+  return JSON.parse(JSON.stringify(param));
+}
+
 export {
   showEllipsis,
   pointer,
@@ -9,4 +22,5 @@ export {
 export default {
   showEllipsis,
   pointer,
+  log,
 };
--- src/utils/pointer.js
+++ src/utils/pointer.js
@@ -1,6 +1,11 @@
 /**
  * 链式调用，避免错误：Uncaught TypeError: Cannot read property 'xxx' of undefined
  * @param obj: 链式调用的对象
+ * 
+ * example
+ * const a = { a: { b: { c : 1 } } };
+ * pointer(a).b.c(); // 1
+ * pointer(a).c.c(); // undefined
  */
 function pointer(obj, path = []) {
   return new Proxy(() => {}, {
